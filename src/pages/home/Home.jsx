0d73743f80a8c34bd74953586c68f9325f613199
ajhar10 <ajharrimon90@gmail.com>
2023-03-26 02:44:15 +0600

    home page design done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,6 +5,7 @@ import Widget from "../../components/widget/Widget";
 import Featured from "../../components/featured/Featured";
 import Chart from "../../components/chart/Chart";
 import "./home.scss";
+import List from "../../components/table/Table";
 
 const Home = () => {
   return (
@@ -21,6 +22,10 @@ const Home = () => {
         <div className="charts">
           <Featured />
           <Chart />
+        </div>
+        <div className="listContainer">
+          <div className="listTitle">Latest Transaction</div>
+          <List />
         </div>
       </div>
     </div>
